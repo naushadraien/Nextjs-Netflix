0ad7fbb972c1ae90c880a8c4db7321c6bdd6665e
naushadraien <naushadraien5@gmail.com>
2023-04-30 23:58:29 +0545

    added some comments in the dynamicId folder file

--- src/app/movie/[dynamicId]/page.jsx
+++ src/app/movie/[dynamicId]/page.jsx
@@ -1,6 +1,6 @@
 
 const page = ({params}) => { //params is basically id 
-    const id = params.dynamicId; //destructuring id from params here params.'dynamicfilename'
+    const id = params.dynamicId; //destructuring id from params here params.'dynamicfoldername which was written in the square bracket'
     console.log(id);
   return (
     <h1>I am Dynamic {id}</h1>
