e562a6ec0d738b436f486afec9e48844639c5e21
naushadraien <naushadraien5@gmail.com>
2023-05-01 14:46:43 +0545

    made dynamic page for all movies, shows etc

--- src/app/movie/[dynamicId]/page.jsx
+++ src/app/movie/[dynamicId]/page.jsx
@@ -1,7 +1,9 @@
+import styles from '@/app/styles/common.module.css'
+import Image from 'next/image';
 
 const page = async ({params}) => { //params is basically id 
     const id = params.dynamicId; //destructuring id from params here params.'dynamicfoldername which was written in the square bracket'
-    const url = `https://netflix54.p.rapidapi.com/title/details/?ids=${id}81&lang=en`; //here id is dynamic and is get from the Rapid API NEXTFLIX Title Details API
+    const url = `https://netflix54.p.rapidapi.com/title/details/?ids=${id}&lang=en`; //here id is dynamic and is get from the Rapid API NEXTFLIX Title Details API
     // console.log("RAPID_URL:", url);
     const options = {
       method: "GET",
@@ -19,7 +21,18 @@ const page = async ({params}) => { //params is basically id
     const mainData = data[0].details; //our main data is in the data[0].details when checked the endpoints of the Title Details of RAPID API NETFLIX API
     // console.log(id);
   return (
-    <h1>I am Dynamic {id}</h1>
+    <div className={styles.container}>
+      <h2 className={styles.movie_title}>Netflix \ <span>{mainData.type}</span></h2>
+      <div className={styles.card_section}>
+        <div>
+          <Image src={mainData.backgroundImage.url} alt={mainData.title} width={600} height={300} />
+        </div>
+        <div>
+          <h1>{mainData.title}</h1>
+          <p>{mainData.synopsis}</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
