b3b38bffc4a6e7c5b044fb47f558c6c4fa0fe2f5
naushadraien <naushadraien5@gmail.com>
2023-05-01 15:37:44 +0545

    added comments in loading.js file

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,5 +1,7 @@
 import styles from '@/app/styles/common.module.css'
 
+//this file is for showing the loading animation when the page is loading and added css for this loading className from loading.io/css
+
 const loading = () => {
   return (
     <section className={styles.loading_section}>
