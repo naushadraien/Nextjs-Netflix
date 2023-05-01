07ce46d94d7aa7a062b7e13d71168a51aab1a2a9
naushadraien <naushadraien5@gmail.com>
2023-05-01 16:18:47 +0545

    added shapeDivider to the heroSection of our app

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -32,6 +32,15 @@ const HeroSection = ({title, imageUrl}) => { //title and imageUrl props passed f
                 </div>
             </div>
         </div>
+        {/* this shapedivider is div and css for this div is taken from the shapedivider.app website */}
+       <div className={heroStyles['custom-shape-divider-bottom-1681647578']}>
+                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120"
+                     preserveAspectRatio="none">
+                    <path
+                        d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+                        className={heroStyles["shape-fill"]}></path> {/* newWay to add the className for the shape-fill i.e it has - between shape and fill so it can be written within the square bracket as written in this line of className */}  
+                </svg>
+            </div>
     </main>
   )
 }
